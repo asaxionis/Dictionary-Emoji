27289b5d0f04c0709b3ceb8ab22fa9fbdb92071b
Andrew Saxionis <asaxionis@bryant.edu>
2016-12-07 21:31:44 -0500

    We have added the beginning of the second page!

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var EmojiTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        EmojiTable.dataSource = self
+        EmojiTable.delegate = self
+        
+        
+    }
+    
+    var emojiList = ["😡", "😍", "😁", "🤗", "🤓"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return emojiList.count
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = emojiList[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
